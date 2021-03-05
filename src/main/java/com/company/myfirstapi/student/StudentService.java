a3dcc44efde4a02987deee7443fd73092ef686bd
Allahverdiyev-Rashad <developer.rashad.allahverdiyev@gmail.com>
2021-03-06 00:50:11 +0400

    DELETE method

--- src/main/java/com/company/myfirstapi/student/StudentService.java
+++ src/main/java/com/company/myfirstapi/student/StudentService.java
@@ -28,4 +28,14 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + studentId + "does not exists");
+        }
+
+        studentRepository.deleteById(studentId);
+    }
 }
